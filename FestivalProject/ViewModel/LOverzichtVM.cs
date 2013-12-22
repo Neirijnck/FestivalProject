c5ddfcb92f5a698c879de633f29d74514c01d62b
Neirijnck <neirijnck@hotmail.com>
2013-12-22 22:30:54 +0100

    Festival Planner V0.13
    - Structuur van overzichtspagina veranderd naar een listbox voor dag en
    stage

--- FestivalProject/ViewModel/LOverzichtVM.cs
+++ FestivalProject/ViewModel/LOverzichtVM.cs
@@ -52,64 +52,6 @@ namespace FestivalProject.ViewModel
             set { _dagen = value; OnPropertyChanged("Dagen"); }
         }
 
-        //Property voor alle dagoverzichten
-        private ObservableCollection<ObservableCollection<LineUp>> _dagOverzichten;
-
-        public ObservableCollection<ObservableCollection<LineUp>> DagOverzichten
-        {
-            get { return _dagOverzichten; }
-            set { _dagOverzichten = value; OnPropertyChanged("DagOverzichten"); }
-        }
-
-        //VOOR DYNAMISCH LINKS TOE TE VOEGEN AAN MODERN UI MENU:
-        //BRON: DISCUSSIONS MUI.CODEPLEX.COM
-        private LinkCollection _links;
-
-        public LinkCollection Links
-        {
-            get { return _links; }
-            set { _links = value; OnPropertyChanged("Links"); }
-        }
-
-        public Link NewLink { get; set; }
-
-        private LinkCollection GenerateLinks()
-        {
-            LinkCollection links = new LinkCollection();
-            for (int i = 0; i <= Dagen.Count; i++)
-            {
-                //Nieuwe usercontrol voor de link
-                UserControl dag = new UserControl();
-                dag.Name = "LODag" + (i + 1);
-                
-
-                //Nieuwe Link
-                NewLink = new Link();
-                NewLink.DisplayName = "Dag " + (i + 1);
-                NewLink.Source = new Uri("/View/"+ dag.Name + ".xaml", UriKind.Relative);
-                links.Add(NewLink);
-
-           
-                
-            }
-            return links;
-        }
-        
-        private Uri _selectedSource;
-
-        public Uri SelectedSource
-        {
-            get { return _selectedSource; }
-            set 
-            {
-                if (this._selectedSource != value)
-                {
-                    this._selectedSource = value; OnPropertyChanged("SelectedSource");
-                }
-            }
-        }
-
-
         //Constructor
         public LOverzichtVM()
         {
@@ -117,8 +59,6 @@ namespace FestivalProject.ViewModel
             Stages = Stage.GetStages();
             SelectedStage = Stages[0];
             Dagen = BerekenData();
-            Links = GenerateLinks();
-            SelectedSource = Links[0].Source;
         }
 
         //Methode om aantal dagen te berekenen tussen begin en einddata
